0a7af8dd2f2c8272f46977ae0c40a29e3844f527
范超威 <597064177@qq.com>
2022-07-04 15:38:02 +0800

    完成 46 实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -10,31 +10,56 @@ export function baseParse(content: string) {
   // 将字符串嵌套在对象中
   const context = createParserContent(content)
 
-  return createRoot(parseChildren(context))
+  return createRoot(parseChildren(context, []))
 }
 
 // 对字符串进行处理
-function parseChildren(context): any {
+// ancestors 是存放 tag 的数组，用于 isEnd 函数的判断
+function parseChildren(context, ancestors): any {
   const nodes: any = []
 
   let node
-  const s = context.source
-  if (s.startsWith('{{')) {
-    // 处理{{}}
-    node = parseInterpolation(context)
-  } else if (s[0] === '<') {
-    if (/[a-z]/i.test(s)) {
-      // 处理 element
-      node = parseElement(context)
+
+  // 循环处理, 为的是将字符串逐一处理。
+  while (!isEnd(context, ancestors)) {
+    const s = context.source
+    if (s.startsWith('{{')) {
+      // 处理{{}}
+      node = parseInterpolation(context)
+    } else if (s[0] === '<') {
+      if (/[a-z]/i.test(s)) {
+        // 处理 element
+        node = parseElement(context, ancestors)
+      }
+    } else {
+      // 处理 text
+      node = parseText(context)
     }
-  } else {
-    // 处理 text
-    node = parseText(context)
+
+    nodes.push(node)
   }
 
-  nodes.push(node)
-
   return nodes
+}
+
+// 是否停止对字符串处理的循环
+function isEnd(context, ancestors) {
+  const s = context.source
+  // 是否是 </***> 结尾
+  // if (ancestors && s.startsWith(`</${ancestors}>`)) {
+  //   return true
+  // }
+
+  for (let i = ancestors.length - 1; i >= 0; i--) {
+    // 循环存放 tag 的数组，当当前处理的字符串与数组内的 tag 存在匹配时，返回 true
+    const tag = ancestors[i]
+    if (startsWithEndTagOpen(s, tag)) {
+      return true
+    }
+  }
+
+  // 字符串是否为空
+  return !s
 }
 
 function parseTextData(context, length) {
@@ -45,11 +70,26 @@ function parseTextData(context, length) {
   return content
 }
 
+function startsWithEndTagOpen(source, tag) {
+  return source.slice(2, 2 + tag.length) === tag
+}
+
 //#region 处理 text
 
 function parseText(context: any) {
-  // text 就直接获取其本身就行
-  const content = parseTextData(context, context.source.length)
+  let endIndex = context.source.length
+  let endToken = ['{{', '<']
+
+  for (let i = 0; i < endToken.length; i++) {
+    // 判断并找出 text 的长度
+    const index = context.source.indexOf(endToken[i])
+    // 判断所有情况，取最小值(最近的)
+    if (index >= 0 && index < endIndex) {
+      endIndex = index
+    }
+  }
+
+  const content = parseTextData(context, endIndex)
 
   return {
     type: NodeTypes.TEXT,
@@ -61,14 +101,27 @@ function parseText(context: any) {
 
 //#region 处理 element
 
-function parseElement(context) {
+function parseElement(context, ancestors) {
   // 处理并获取需要 return 的对象
-  const element = parseTag(context, TagType.START)
+  const element: any = parseTag(context, TagType.START)
 
-  // 再次处理是为了消除 element 的后半部分，例：</div>
-  parseTag(context, TagType.END)
+  // 将当前处理的 tag 存入数组。用于 isEnd 判断使用
+  ancestors.push(element.tag)
 
-  return element
+  // 对标签内的数据进行解析
+  element.children = parseChildren(context, ancestors)
+
+  // 标签内部数据处理完成，删除该标签的判断用标识。
+  ancestors.pop()
+
+  // 判断后半段是否和 tag 相同。如果是则是完整标签正常消除。如果不是说明标签存在确实，抛出异常。
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    // 再次处理是为了消除 element 的后半部分，例：</div>
+    parseTag(context, TagType.END)
+    return element
+  } else {
+    throw `缺少结束标签：${element.tag}`
+  }
 }
 
 function parseTag(context, type: TagType) {
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -23,16 +23,76 @@ describe('Parse', () => {
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.ELEMENT,
         tag: 'div',
+        children: [],
       })
     })
   })
 
   describe('text', () => {
-    const ast = baseParse('some text')
+    test('some text', () => {
+      const ast = baseParse('some text')
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.TEXT,
+        content: 'some text',
+      })
+    })
+  })
+
+  test('hello world', () => {
+    const ast = baseParse('<div>hi,{{message}}</div>')
 
     expect(ast.children[0]).toStrictEqual({
-      type: NodeTypes.TEXT,
-      content: 'some text',
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: 'hi,',
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message',
+          },
+        },
+      ],
     })
+  })
+
+  test('nested element', () => {
+    const ast = baseParse('<div><p>hi,</p>{{message}}</div>')
+
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.ELEMENT,
+          tag: 'p',
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: 'hi,',
+            },
+          ],
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message',
+          },
+        },
+      ],
+    })
+  })
+
+  test('should throw error when lack end tag', () => {
+    // baseParse('<div><span></div>')
+    expect(() => {
+      baseParse('<div><span></div>')
+    }).toThrow('缺少结束标签：span')
   })
 })
