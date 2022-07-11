855a4d0fe77b3a7be788b9eda3eb757482691baf
范超威 <597064177@qq.com>
2022-07-11 13:45:33 +0800

    完成51章，实现代码生成三种联合类型

--- src/compiler-core/src/ast.ts
+++ src/compiler-core/src/ast.ts
@@ -1,3 +1,5 @@
+import { CREATE_ELEMENT_BLOCK } from './runtimeHelpers'
+
 export const enum NodeTypes {
   INTERPOLATION, // 插值类型
   SIMPLE_EXPRESSION, // 插值内的内容
@@ -8,7 +10,8 @@ export const enum NodeTypes {
 }
 
 //transformElement 中 element 类型最后的输出对象
-export function createVNodeCall(tag, props, children) {
+export function createVNodeCall(context, tag, props, children) {
+  context.helper(CREATE_ELEMENT_BLOCK)
   return {
     type: NodeTypes.ELEMENT,
     tag,
--- src/compiler-core/src/transforms/transformElement.ts
+++ src/compiler-core/src/transforms/transformElement.ts
@@ -1,13 +1,10 @@
 import { createVNodeCall, NodeTypes } from '../ast'
-import { CREATE_ELEMENT_BLOCK } from '../runtimeHelpers'
 
 // element 的情况下，添加引用
 export function transformElement(node, context) {
   if (node.type === NodeTypes.ELEMENT) {
     // return 一个函数是因为需要此插件倒序的时候执行
     return () => {
-      context.helper(CREATE_ELEMENT_BLOCK)
-
       // tag
       const vnodeTag = `'${node.tag}'`
 
@@ -19,7 +16,12 @@ export function transformElement(node, context) {
       const vnodeChildren = children[0]
 
       // 如果是 element 类型则生成特殊的对象
-      node.codegenNode = createVNodeCall(vnodeTag, vnodeProps, vnodeChildren)
+      node.codegenNode = createVNodeCall(
+        context,
+        vnodeTag,
+        vnodeProps,
+        vnodeChildren
+      )
     }
   }
 }
